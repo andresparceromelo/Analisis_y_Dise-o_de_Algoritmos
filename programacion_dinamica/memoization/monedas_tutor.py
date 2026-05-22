f5da8f0fbd464444ee546dd6b12c37b91144eb7d
Andres <bedoyacanoandres@gmail.com>
2026-05-22 08:11:08 -0500

    variaciones y solucion de pintar_casas

--- programacion_dinamica/memoization/monedas_tutor.py
+++ programacion_dinamica/memoization/monedas_tutor.py
@@ -1,27 +1,27 @@
-def resolve_coin_change_problem(coins, x):
-    if len(coins) == 0:
-        return -1
-    
+def vender_varilla(v,longitud,precio):
+    n = len(longitud) 
+
     memo = {}
 
-    def dp(i):
-        if i == 0:
+    def dp(i,v):
+        
+        if i == n or v == 0:
             return 0
-        if i < 0:
-            return float('inf')
-            
-        if i in memo:
-            return memo[i]
-            
-        min_monedas = float('inf')
-        for coin in coins:
-            resultado = 1 + dp(i - coin)
-            min_monedas = min(min_monedas, resultado)
-            
-        memo[i] = min_monedas
-        return min_monedas
 
-    respuesta = dp(x)
-    return respuesta if respuesta != float('inf') else -1
+        if (i,v) in memo:
+            return memo[(i,v)]
+        
+        no_tomar = dp(i+1, v)
+        
+        tomar = 0
+        if v >= longitud[i]:
+            tomar = precio[i] + dp(i, v - longitud[i])
+        
+        
+        
+        memo[(i,v)] = max(no_tomar, tomar)
 
-print(resolve_coin_change_problem([1, 3, 4], 6))
+        return memo[(i,v)]
+    return dp(0, v)
+
+print(vender_varilla(4,[1,2,3,4],[1,5,8,9]))
